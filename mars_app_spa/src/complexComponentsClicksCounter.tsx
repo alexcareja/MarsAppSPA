cf2cfceb61719428146108c736275cc5094462d5
Alex Careja <Alex.Careja@softwire.com>
2021-07-01 16:42:42 +0300

    added context to components replacing props

--- mars_app_spa/src/complexComponentsClicksCounter.tsx
+++ mars_app_spa/src/complexComponentsClicksCounter.tsx
@@ -1,28 +1,39 @@
+import React, { useContext } from "react"
 import react, { useState, useEffect } from "react"
 
-const Component4: React.FC<{c4count: number}> = (props) => {
+interface CounterContext {
+    clickCount: number;
+    incrementCount: () => void;
+}
 
+const ButtonContext = React.createContext<CounterContext>({
+    clickCount: 0,
+    incrementCount: () => {}
+});
+
+const Component4: React.FC = () => {
+    const context = useContext(ButtonContext);
     return (
         <div>
-            <p>{props.c4count}</p>
+            <p>{context.clickCount}</p>
         </div>
     )
 }
 
-
-const Component3: React.FC<{c3count: number}> = (props)=> {
+const Component3: React.FC = () => {
     return (
         <div>
             <p>Hello from component 3</p>
-            <Component4 c4count = {props.c3count}/>
+            <Component4/>
         </div>
-    )
+    );
 }
 
-const Component2: React.FC<{incrementFunc: () => void}> = (props) => {
+const Component2: React.FC = ()  => {
+    const context = useContext(ButtonContext);
     return (
         <div>
-            <button onClick={props.incrementFunc}>Click me!</button>
+            <button onClick={context.incrementCount}>Click me!</button>
         </div>
     );
 }
@@ -44,10 +55,12 @@ const Component1: React.FC = () => {
 
     return (
         <div>
-            <Component2 incrementFunc = {incrementCount}/>
-            <Component3 c3count = {clickCount}/>
+            <ButtonContext.Provider value={{ clickCount, incrementCount}}>
+                <Component2/>
+                <Component3/>
+            </ButtonContext.Provider>
         </div>
-    )
+    );
 }
 
 export { Component1 }
