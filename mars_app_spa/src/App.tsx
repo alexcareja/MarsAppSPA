431fc78559d63c01ddd4a7876619c59839c06602
Alex Careja <Alex.Careja@softwire.com>
2021-07-01 13:05:09 +0300

    restored main branch to its initial state

--- mars_app_spa/src/App.tsx
+++ mars_app_spa/src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { NasaInformations } from "./nasaIntro";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <NasaInformations/>
+        <p>Hello World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
